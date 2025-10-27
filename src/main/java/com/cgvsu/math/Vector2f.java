e655c891073471cff1b64530d76f0a2b777440fa
observernear <lapinikita@mail.ru>
2025-10-27 12:38:47 +0300

    Добавил пакет com.cgvsu.math аналог(javax.vecmath)

--- src/main/java/com/cgvsu/math/Vector2f.java
+++ src/main/java/com/cgvsu/math/Vector2f.java
@@ -1,11 +1,34 @@
 package com.cgvsu.math;
 
-// Это заготовка для собственной библиотеки для работы с линейной алгеброй
-public class Vector2f {
+import com.cgvsu.math.base.AbstractVector;
+
+public class Vector2f extends AbstractVector<Vector2f> {
+
     public Vector2f(float x, float y) {
-        this.x = x;
-        this.y = y;
+        super(new float[]{x, y});
     }
 
-    public float x, y;
+    @Override
+    protected Vector2f createNew(float[] components) {
+        return new Vector2f(components[0], components[1]);
+    }
+
+    public float getX() { return components[0]; }
+    public float getY() { return components[1]; }
+
+    public float distance(Vector2f other) { // расстояние
+
+        float dx = getX() - other.getX();
+        float dy = getY() - other.getY();
+
+        return (float) Math.sqrt(dx * dx + dy * dy);
+
+    }
+
+    @Override
+    public String toString() {
+
+        return String.format("(%.4f, %.4f)", getX(), getY());
+
+    }
 }
