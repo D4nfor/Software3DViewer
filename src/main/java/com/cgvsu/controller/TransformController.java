e9249e88efc335b08987e62ea91a5d71260a4fd2
observernear <lapinikita@mail.ru>
2025-12-29 10:49:47 +0300

    Tets

--- src/main/java/com/cgvsu/controller/TransformController.java
+++ src/main/java/com/cgvsu/controller/TransformController.java
@@ -6,6 +6,7 @@ import com.cgvsu.render_engine.Transform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.layout.VBox;
 
 public class TransformController {
@@ -96,5 +97,115 @@ public class TransformController {
     public void hidePanel() {
         transformPanel.setVisible(false);
         transformPanel.setManaged(false);
+    }
+
+    // ОСТОРОЖНО НИЖЕ КОСТЫЛИ
+    public void setScaleX(float value) {
+        if (scaleXField != null) {
+            SpinnerValueFactory<Double> factory = scaleXField.getValueFactory();
+            if (factory != null) {
+                factory.setValue((double) value);
+            } else {
+                scaleXField.getEditor().setText(String.valueOf(value));
+            }
+        }
+    }
+
+    public void setScaleY(float value) {
+        if (scaleYField != null) {
+            SpinnerValueFactory<Double> factory = scaleYField.getValueFactory();
+            if (factory != null) {
+                factory.setValue((double) value);
+            } else {
+                scaleYField.getEditor().setText(String.valueOf(value));
+            }
+        }
+    }
+
+    public void setScaleZ(float value) {
+        if (scaleZField != null) {
+            SpinnerValueFactory<Double> factory = scaleZField.getValueFactory();
+            if (factory != null) {
+                factory.setValue((double) value);
+            } else {
+                scaleZField.getEditor().setText(String.valueOf(value));
+            }
+        }
+    }
+
+    public void setRotateX(float value) {
+        if (rotateXField != null) {
+            SpinnerValueFactory<Double> factory = rotateXField.getValueFactory();
+            if (factory != null) {
+                double normalized = value % 360;
+                if (normalized < 0) normalized += 360;
+                factory.setValue(normalized);
+            } else {
+                rotateXField.getEditor().setText(String.valueOf(value));
+            }
+        }
+    }
+
+    public void setRotateY(float value) {
+        if (rotateYField != null) {
+            SpinnerValueFactory<Double> factory = rotateYField.getValueFactory();
+            if (factory != null) {
+                double normalized = value % 360;
+                if (normalized < 0) normalized += 360;
+                factory.setValue(normalized);
+            } else {
+                rotateYField.getEditor().setText(String.valueOf(value));
+            }
+        }
+    }
+
+    public void setRotateZ(float value) {
+        if (rotateZField != null) {
+            SpinnerValueFactory<Double> factory = rotateZField.getValueFactory();
+            if (factory != null) {
+                double normalized = value % 360;
+                if (normalized < 0) normalized += 360;
+                factory.setValue(normalized);
+            } else {
+                rotateZField.getEditor().setText(String.valueOf(value));
+            }
+        }
+    }
+
+    public void setTranslateX(float value) {
+        if (translateXField != null) {
+            SpinnerValueFactory<Double> factory = translateXField.getValueFactory();
+            if (factory != null) {
+                factory.setValue((double) value);
+            } else {
+                translateXField.getEditor().setText(String.valueOf(value));
+            }
+        }
+    }
+
+    public void setTranslateY(float value) {
+        if (translateYField != null) {
+            SpinnerValueFactory<Double> factory = translateYField.getValueFactory();
+            if (factory != null) {
+                factory.setValue((double) value);
+            } else {
+                translateYField.getEditor().setText(String.valueOf(value));
+            }
+        }
+    }
+
+    public void setTranslateZ(float value) {
+        if (translateZField != null) {
+            SpinnerValueFactory<Double> factory = translateZField.getValueFactory();
+            if (factory != null) {
+                factory.setValue((double) value);
+            } else {
+                translateZField.getEditor().setText(String.valueOf(value));
+            }
+        }
+    }
+
+    public void applyTransform() {
+        handleTransformChange();
     }
 }
